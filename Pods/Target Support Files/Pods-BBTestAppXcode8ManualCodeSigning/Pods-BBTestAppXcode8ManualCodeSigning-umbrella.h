76793db75f9dd0246f28f1ad34b983dd8be55d5d
Christopher Stott <christopherstott@gmail.com>
2016-09-29 07:17:10 -0700

    Pods

---
+++ Pods/Target Support Files/Pods-BBTestAppXcode8ManualCodeSigning/Pods-BBTestAppXcode8ManualCodeSigning-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BBTestAppXcode8ManualCodeSigningVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BBTestAppXcode8ManualCodeSigningVersionString[];
+
